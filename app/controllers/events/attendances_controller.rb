358b281c955308476dbea819d2ae883705ed89c7
toyohiroppp <fix.jpmoy@gmail.com>
2023-07-23 11:32:00 +0900

    事業をエンジニアリングしよう２実装　テスト通過

--- app/controllers/events/attendances_controller.rb
+++ app/controllers/events/attendances_controller.rb
@@ -3,11 +3,15 @@
 class Events::AttendancesController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
-    event_attendance = current_user.attend(@event)
-    (@event.attendees - [current_user] + [@event.user]).uniq.each do |user|
-      NotificationFacade.attended_to_event(event_attendance, user)
+    if @event.only_woman && current_user.gender != '女性'
+      redirect_back(fallback_location: root_path, success: '女性専用のため参加できません')
+    else
+      event_attendance = current_user.attend(@event)
+      (@event.attendees - [current_user] + [@event.user]).uniq.each do |user|
+        NotificationFacade.attended_to_event(event_attendance, user)
+      end
+      redirect_back(fallback_location: root_path, success: '参加の申込をしました')
     end
-    redirect_back(fallback_location: root_path, success: '参加の申込をしました')
   end
 
   def destroy
